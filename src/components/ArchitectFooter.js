5c7a0e9718d354d8f63f3f7e46ad1803b3fcc8c6
Phil Guinchard <phil.guinchard@gmail.com>
2019-08-07 22:43:35 -0400

    Fixing a footer display issue on small phones

--- src/components/ArchitectFooter.js
+++ src/components/ArchitectFooter.js
@@ -16,23 +16,23 @@ export default class ArchitectFooter extends Component {
                 <FooterTab style={{justifyContent: 'center', backgroundColor: '#f57e20'}}>
                     <Button vertical onPress={() => this.props.navigation.navigate('NewTemplate', {from: 'Architect'})}>
                         <Icon type='FontAwesome' name='file' style={{color: '#FFF'}} />
-                        <Text uppercase={false} style={{color: '#FFF'}}>New</Text>
+                        <Text uppercase={false} style={{fontSize: 10, color: '#FFF'}}>New</Text>
                     </Button>
                     <Button vertical onPress={() => file.saveTemplate(this.props.template)}>
                         <Icon type='FontAwesome' name='save' style={{color: '#FFF'}} />
-                        <Text uppercase={false} style={{color: '#FFF'}}>Save</Text>
+                        <Text uppercase={false} style={{fontSize: 10, color: '#FFF'}}>Save</Text>
                     </Button>
                     <Button vertical onPress={() => this.props.navigation.navigate('OpenTemplate')}>
                         <Icon type='FontAwesome' name='folder-open' style={{color: '#FFF'}}/>
-                        <Text uppercase={false} style={{color: '#FFF'}}>Open</Text>
+                        <Text uppercase={false} style={{fontSize: 10, color: '#FFF'}}>Open</Text>
                     </Button>
                     <Button vertical onPress={() => file.importTemplate(() => {}, () => {})}>
                         <Icon type='FontAwesome' name='download' style={{color: '#FFF'}}/>
-                        <Text uppercase={false} style={{color: '#FFF'}}>Import</Text>
+                        <Text uppercase={false} style={{fontSize: 10, color: '#FFF'}}>Import</Text>
                     </Button>
                     <Button vertical onPress={() => this.props.navigation.navigate('TemplateDelete')}>
                         <Icon type='FontAwesome' name='trash' style={{color: '#FFF'}}/>
-                        <Text uppercase={false} style={{color: '#FFF'}}>Delete</Text>
+                        <Text uppercase={false} style={{fontSize: 10, color: '#FFF'}}>Delete</Text>
                     </Button>
                 </FooterTab>
             </Footer>
