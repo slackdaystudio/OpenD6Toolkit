b6af3373fcef5466544ec5f7ce6f9fb9e9d1f27e
Phil Guinchard <phil.guinchard@gmail.com>
2023-10-04 19:25:44 -0400

    Misc config changes

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: ['./assets/fonts/'],
     dependencies: {
         'react-native-vector-icons': {
             platforms: {
