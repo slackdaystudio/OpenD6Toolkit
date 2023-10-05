a3d14becb512c26cfe00edfcfa3459fb677837d9
Phil Guinchard <sentry0@gmail.com>
2023-10-04 22:27:43 -0400

    Fixing the double splash screen on android

--- android/app/src/main/java/com/opend6toolkit/MainActivity.java
+++ android/app/src/main/java/com/opend6toolkit/MainActivity.java
@@ -10,7 +10,8 @@ import android.os.Bundle;
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this); 
+        SplashScreen.show(this, true);
         super.onCreate(savedInstanceState);
     }
 
